3acb6faa6a6f772deb3e1ea1f9e306041fbc778b
ianfleming464 <ianflemingdeveloper@gmail.com>
2022-12-02 22:13:27 +0100

    Fetch requests, dropdown search menu

--- starter-files/src/App.tsx
+++ starter-files/src/App.tsx
@@ -1,10 +1,24 @@
 import { ChangeEvent, useState } from 'react'
-// CALL EXAMPLE: api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+// CALL EXAMPLE: https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
 const App = (): JSX.Element => {
   const [searchTerm, setSearchTerm] = useState<string>('')
+  const [options, setOptions] = useState<[]>([])
+
+  const getSearchOptions = (value: string) => {
+    fetch(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${value.trim()}&limit=5&appid=${
+        process.env.REACT_APP_API_KEY
+      }`
+    )
+      .then((res) => res.json())
+      .then((data) => setOptions(data))
+  }
 
   const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setSearchTerm(e.target.value)
+    const value = e.target.value.trim()
+    setSearchTerm(value)
+    if (value === '') return
+    getSearchOptions(value)
   }
 
   return (
@@ -14,13 +28,20 @@ const App = (): JSX.Element => {
           Weather <span className="font-black">Forecast</span>
         </h1>
         <p className="text-sm mt-2">Select your destination</p>
-        <div className="flex mt-10 md:mt-4">
+
+        <div className="relative flex mt-10 md:mt-4">
           <input
             type="text"
             value={searchTerm}
             className="px-2 py-1 rounded-l-md border-2 border-white"
             onChange={onInputChange}
           />
+
+          <ul className="absolute top-9 bg-white m1-1 rounded-b-md">
+            {options.map((option: { name: string }) => (
+              <p>{option.name}</p>
+            ))}
+          </ul>
           <button className="rounded-r-md border-2 border-zinc-100 hover:border-zinc-500 hover:text-zinc-500 text-zinc-100 px-2 py-1 cursor-pointer">
             Search me
           </button>
