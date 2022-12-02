958b281674ddf1c456167c2a1a5ccdfead5e1279
ianfleming464 <ianflemingdeveloper@gmail.com>
2022-12-02 15:30:40 +0100

    Adding input functionality, state

--- starter-files/src/App.tsx
+++ starter-files/src/App.tsx
@@ -1,4 +1,12 @@
-const App = () => {
+import { ChangeEvent, useState } from 'react'
+// CALL EXAMPLE: api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+const App = (): JSX.Element => {
+  const [searchTerm, setSearchTerm] = useState<string>('')
+
+  const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value)
+  }
+
   return (
     <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
       <section className="w-full md:max-w-[500px] p-4 flex flex-col text-center items-center justify-center md:px-10 lg:p-24 h-full lg:h-[500px] bg-white bg-opacity-20 backdrop-blur-ls rounded drop-shadow-lg text-zinc-700">
@@ -9,8 +17,9 @@ const App = () => {
         <div className="flex mt-10 md:mt-4">
           <input
             type="text"
-            value=""
+            value={searchTerm}
             className="px-2 py-1 rounded-l-md border-2 border-white"
+            onChange={onInputChange}
           />
           <button className="rounded-r-md border-2 border-zinc-100 hover:border-zinc-500 hover:text-zinc-500 text-zinc-100 px-2 py-1 cursor-pointer">
             Search me
